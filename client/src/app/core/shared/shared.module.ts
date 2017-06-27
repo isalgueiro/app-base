391e41a9035a2f027ff59ef9d6047e91185b2593
Alberto Basalo <albertobasalo@hotmail.com>
2017-06-27 16:24:27 +0200

    Move load_panel and editor to layouts and toast to widgets closes #38

--- client/src/app/core/shared/shared.module.ts
+++ client/src/app/core/shared/shared.module.ts
@@ -10,15 +10,15 @@ import { CounterComponent } from './widgets/counter/counter.component';
 import { RouterModule } from '@angular/router';
 import { TileComponent } from './widgets/tile/tile.component';
 import { ModalComponent } from './layouts/modal/modal.component';
-import { ToastComponent } from './toast/toast.component';
+import { ToastComponent } from './widgets/toast/toast.component';
 import { PanelComponent } from './widgets/panel/panel.component';
 import { DashboardComponent } from './layouts/dashboard/dashboard.component';
 import { SelectComponent } from './forms/select/select.component';
 import { RadioComponent } from './forms/radio/radio.component';
 import { SwitchComponent } from './forms/switch/switch.component';
 import { TextareaComponent } from './forms/textarea/textarea.component';
-import { LoadEmptyStateComponent } from './load-empty-state/load-empty-state.component';
-import { EditorComponent } from './editor/editor.component';
+import { LoadEmptyStateComponent } from './layouts/load-empty-state/load-empty-state.component';
+import { EditorComponent } from './layouts/editor/editor.component';
 import { ActionComponent } from './layouts/action/action.component';
 
 @NgModule({
