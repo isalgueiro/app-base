01be77e0688c739fedf504b728e36d0731266153
silvelo <arturo.silvelo@gmail.com>
2017-06-28 17:27:17 +0200

    Upload new enviroments fix #45

---
+++ server/src/environments/environment.ts
@@ -0,0 +1,28 @@
+import { STAGES } from './../app/core/shared/enums';
+import { ISettings } from './../app/core/shared/models';
+
+const settings: ISettings = {
+  env: STAGES[process.env.env] || STAGES.dev,
+  port: process.env.port || 2000,
+  secret: process.env.port || 'secret',
+  database: {
+    database: process.env.database_name || 'bookings',
+    username: process.env.database_username || '',
+    password: process.env.database_password || '',
+    host: process.env.database_host || 'localhost',
+    port: process.env.database_port || 27017,
+  },
+  mailerSettings: {
+    port: process.env.mailer_port || 0,
+    host: process.env.mailer_host || "",
+    secure: process.env.mailer_secure || false,
+    ignoreTLS: process.env.mailer_ignoreTLS || true,
+    auth: {
+      user: process.env.mailer_user || "",
+      pass: process.env.mailer_pass || ""
+    },
+  },
+  path: process.env.prod_path || '',
+};
+
+export const SETTINGS = settings;
