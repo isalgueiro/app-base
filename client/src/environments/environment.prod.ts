c90a8c4cdca4f024e0e27769e9dcda829e581dd8
silvelo <arturo.silvelo@gmail.com>
2017-07-12 12:59:59 +0200

    #47

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: 'AppBase',
+  appTitle: 'App Base',
+  apiUrl: 'http://46.101.96.140:2000/api/',
+  assetsUrl: 'http://46.101.96.140:4200/',
+  secret: 'secret',
+  godEmail: 'admin@agorabinaria.com'
 };
