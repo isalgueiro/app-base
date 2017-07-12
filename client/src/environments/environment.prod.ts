1ac416f6fd0e1be1aebab46053c37a92b526210b
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-07-12 13:53:19 +0200

    environments #47

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   appName: 'AppBase',
   appTitle: 'App Base',
-  apiUrl: 'http://46.101.96.140:2000/api/',
+  apiUrl: 'http://46.101.96.140:2323/api/',
   assetsUrl: 'http://46.101.96.140:4200/',
   secret: 'secret',
   godEmail: 'admin@agorabinaria.com'
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -7,8 +7,20 @@ export const environment = {
   production: false,
   appName: 'AppBase',
   appTitle: 'App Base',
+  apiUrl: 'http://beta.appbase.agorabinaria.com/api/',
+  assetsUrl: 'http://localhost:4200/',
+  secret: 'secret',
+  godEmail: 'admin@agorabinaria.com'
+};
+
+/*
+export const environment = {
+  production: false,
+  appName: 'AppBase',
+  appTitle: 'App Base',
   apiUrl: 'http://localhost:2000/api/',
   assetsUrl: 'http://localhost:4200/',
   secret: 'secret',
   godEmail: 'admin@agorabinaria.com'
 };
+*/
