14aa9ac4d0df7c3b469c4e0a8a29ca30ebaa14d4
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-07-14 10:45:38 +0200

    move getValueFromPath from table to utilService #55

--- client/src/app/core/shared/_data/schema.service.ts
+++ client/src/app/core/shared/_data/schema.service.ts
@@ -10,4 +10,40 @@ export class SchemaService {
       .get(`assets/schemas/${schemaName}.json`)
       .map(res => res.json());
   }
+
+  valueByPath(target, path) {
+    if (!path) {
+      return '';
+    }
+    path = path.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    path = path.replace(/^\./, '');           // strip a leading dot
+    const chunks = path.split('.');
+    for (let index = 0, n = chunks.length; index < n; ++index) {
+      const chunk = chunks[index];
+      if (target) {
+        if (chunk in target) {
+          target = target[chunk];
+        } else {
+          return;
+        }
+      } else {
+        return;
+      }
+    }
+    return target;
+  }
+
+  orderDataByKey(values: any[], orderKey: any) {
+    return values.sort((a, b) => {
+      const valA = this.valueByPath(a, orderKey);
+      const valB = this.valueByPath(b, orderKey);
+      if (valA < valB) {
+        return -1;
+      }
+      if (valA > valB) {
+        return 1;
+      }
+      return 0
+    });
+  }
 }
