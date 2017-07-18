210970ba94b91555ea65d30c864316eca1f5ea1e
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-07-18 14:27:56 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   appName: 'AppBase',
   appTitle: 'App Base',
-  apiUrl: 'http://46.101.96.140:2323/api/',
-  assetsUrl: 'http://46.101.96.140:4200/',
+  apiUrl: 'http://beta.appbase.agorabinaria.com/api/',
+  assetsUrl: 'http://beta.appbase.agorabinaria.com/',
   secret: 'secret',
   godEmail: 'admin@agorabinaria.com'
 };
