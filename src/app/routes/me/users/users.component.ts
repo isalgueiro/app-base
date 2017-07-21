ffbfca8a0d889e24ab33f02a4e1ab0b3e05731ac
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-07-21 10:44:51 +0200

    metadata to users component schema #73

--- src/app/routes/me/users/users.component.ts
+++ src/app/routes/me/users/users.component.ts
@@ -10,7 +10,7 @@ import { ROLE, STATUS } from 'app/core/shared/_data/user.model';
   styles: []
 })
 export class UsersComponent implements OnInit {
-actionSchema: IWidgetSchema;
+  actionSchema: IWidgetSchema;
   reportSchema: IReportSchema;
   createFormSchema: IFormSchema;
   users: any[];
@@ -20,19 +20,22 @@ actionSchema: IWidgetSchema;
     this.bus
       .getPageSchema$()
       .takeWhile(() => this.actionSchema == null)
+      // tslint:disable-next-line:cyclomatic-complexity
       .subscribe(schemas => {
-        if (schemas && schemas.actions) {
-          this.actionSchema = schemas.actions;
-          this.createFormSchema = schemas.create;
-          this.reportSchema = schemas.report;
-          this.getUsers();
+        if (schemas && schemas.metadata && schemas.metadata.name === 'users') {
+          if (schemas && schemas.actions) {
+            this.actionSchema = schemas.actions;
+            this.createFormSchema = schemas.create;
+            this.reportSchema = schemas.report;
+            this.getUsers();
+          }
         }
       });
   }
 
   getUsers() {
     this.me.getAdministratedUsers().subscribe(
-      users=> this.users=users);
+      users => this.users = users);
   }
 
 }
